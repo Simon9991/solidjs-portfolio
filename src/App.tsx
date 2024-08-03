c691482f986ada853a363c41d4216a63be87f7eb
Simon9991 <simon.bernabeu@epitech.eu>
2024-08-03 22:08:34 +0900

    feat(App.tsx): made scrolling fading the `header` away

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,64 @@
-import { createSignal, type Component } from "solid-js";
-import { FiChevronsDown } from "solid-icons/fi";
+import { onCleanup, type Component, onMount } from 'solid-js';
 
-import logo from "./logo.svg";
-import styles from "./App.module.scss";
+import styles from './App.module.scss';
+import TechStack from './components/TechStack';
 
 const App: Component = () => {
-  // const [scroll, setScroll] = createSignal(0);
-  // const maxScroll = 100;
-  // let throttleTimeout: number | null;
+    let greetingDiv: HTMLElement | null = null;
+    let header: HTMLElement | null = null;
+    let throttleTimeout: number | null = null;
 
-  // function handleScroll() {
-  //   if (throttleTimeout) return;
+    function handleScroll() {
+        if (throttleTimeout || !greetingDiv || !header) return;
 
-  //   throttleTimeout = setTimeout(() => {
-  //     setScroll(window.scrollY);
-  //     throttleTimeout = null;
-  //   }, 25); // 25ms delay
-  // }
+        throttleTimeout = setTimeout(() => {
+            if (!greetingDiv || !header) return;
 
-  // window.addEventListener("scroll", handleScroll);
-  // onCleanup(() => {
-  //   window.removeEventListener("scroll", handleScroll);
-  // });
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            const maxScroll = header?.clientHeight;
 
-  return (
-    <div id="app-div" class={styles.App}>
-      <header class={styles.header}>
-        <p>Hey! I am</p>
-        <h1>Simon Bernabeu</h1>
-        <p>a software engineer currently living in South Korea</p>
+            const startFadingAt = 70; // Start fading after scrolling 100px
+            const endFadingAt = 170; // Fully transparent after scrolling 300px
 
-        <p class={styles.scrollText}>
-          Scroll down to see more <FiChevronsDown size={22} />
-        </p>
-      </header>
-    </div>
-  );
+            if (scrollTop > startFadingAt) {
+                const opacity = 1 - (scrollTop - startFadingAt) / (endFadingAt - startFadingAt);
+                header.style.opacity = Math.max(opacity, 0).toString();
+            } else {
+                header.style.opacity = '1';
+            }
+            throttleTimeout = null;
+        }, 25); // 25ms delay
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    onMount(() => {
+        greetingDiv = document.getElementById('div-greeting');
+        header = document.getElementById('main-header');
+    });
+
+    onCleanup(() => {
+        window.removeEventListener('scroll', handleScroll);
+    });
+
+    return (
+        <div id="app-div" class={styles.App}>
+            <header id="main-header" class={styles.header}>
+                <div id="div-greeting" class={styles.greeting}>
+                    <p>Hey! I am</p>
+                    <h1>Simon Bernabeu</h1>
+                    <p>a software engineer currently living in South Korea</p>
+                </div>
+
+                <div class={styles.scrollText}>
+                    <p>Scroll down to see more</p>
+                    {/* <FiChevronsRight size={23} class="rotate-90" /> */}
+                </div>
+            </header>
+
+            <TechStack />
+        </div>
+    );
 };
 
 export default App;
