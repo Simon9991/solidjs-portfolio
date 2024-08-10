e8f46541f76c0be056ee67d5bd530c9e3a4a1893
Simon9991 <simon.bernabeu@epitech.eu>
2024-08-10 18:41:25 +0200

    feat(TechStack): showcased some languages

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,48 +1,70 @@
-import type { Component } from 'solid-js';
+import { For, type Component } from 'solid-js';
 
 import styles from '../styles/TechStack.module.scss';
 
+interface StackDetailsProps {
+    label: string;
+    link: string;
+    alt: string;
+}
+
+const StackDetails: Component<StackDetailsProps> = ({ label, link, alt }) => {
+    return (
+        <div class={styles.stackItem}>
+            <img src={link} alt={alt} />
+            <p>{label}</p>
+        </div>
+    );
+};
+
 const TechStack: Component = () => {
+    interface Stack {
+        key: number;
+        label: string;
+        link: string;
+        alt: string;
+    }
+
+    const stack: Stack[] = [
+        {
+            key: 0,
+            label: 'Rust',
+            link: 'https://img.icons8.com/?size=100&id=B1VZ9ZRl8Vwt&format=png&color=000000',
+            alt: 'Rust',
+        },
+        {
+            key: 1,
+            label: 'React',
+            link: 'https://img.icons8.com/color/96/000000/react-native.png',
+            alt: 'React',
+        },
+        {
+            key: 2,
+            label: 'JavaScript & TypeScript',
+            link: 'https://img.icons8.com/color/96/000000/typescript.png',
+            alt: 'JavaScript - TypeScript',
+        },
+        {
+            key: 3,
+            label: 'HTML',
+            link: 'https://img.icons8.com/color/96/000000/html-5.png',
+            alt: 'HTML',
+        },
+        {
+            key: 4,
+            label: 'CSS',
+            link: 'https://img.icons8.com/color/96/000000/css3.png',
+            alt: 'CSS',
+        },
+    ];
+
     return (
         <div class={styles.TechStack}>
             <h2>Tech Stack</h2>
-            <div class="flex flex-wrap justify-center mt-4">
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img
-                        src="https://img.icons8.com/?size=100&id=B1VZ9ZRl8Vwt&format=png&color=000000"
-                        alt="Rust"
-                    />
-                    <p class="text-lg font-semibold">Rust</p>
-                </div>
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img
-                        src="https://img.icons8.com/color/96/000000/react-native.png"
-                        alt="React"
-                    />
-                    <p class="text-lg font-semibold">React</p>
-                </div>
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img
-                        src="https://img.icons8.com/color/96/000000/javascript.png"
-                        alt="JavaScript"
-                    />
-                    <p class="text-lg font-semibold">JavaScript</p>
-                </div>
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img
-                        src="https://img.icons8.com/color/96/000000/typescript.png"
-                        alt="TypeScript"
-                    />
-                    <p class="text-lg font-semibold">TypeScript</p>
-                </div>
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img src="https://img.icons8.com/color/96/000000/html-5.png" alt="HTML" />
-                    <p class="text-lg font-semibold">HTML</p>
-                </div>
-                <div class="flex flex-col items-center justify-center w-1/3 p-4">
-                    <img src="https://img.icons8.com/color/96/000000/css3.png" alt="CSS" />
-                    <p class="text-lg font-semibold">CSS</p>
-                </div>
+            <div class={styles.stackCarrousel}>
+                <For each={stack}>
+                    {({ label, link, alt }) => <StackDetails label={label} link={link} alt={alt} />}
+                </For>
             </div>
         </div>
     );
