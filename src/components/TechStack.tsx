8e3f18dc02b7269378c1c51e4e70d76e087470fa
Simon9991 <simon.bernabeu@epitech.eu>
2024-09-06 23:17:00 +0900

    feat(TechStack): finished (kind of) the tech cards

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -6,19 +6,21 @@ interface Stack {
     label: string;
     link: string;
     alt: string;
+    projects: string[];
 }
 
-const StackDetails: Component<Stack> = ({ label, link, alt }) => {
+const StackDetails: Component<Stack> = ({ label, link, alt, projects }) => {
     return (
         <a class={styles.stackItem} href="/">
+                <h3>{label}</h3>
             <div class={styles.imgWrapper}>
                 <img src={link} alt={alt} />
             </div>
-            <ul>
-                <li>Frontend</li>
-                <li>Backend</li>
-                <li>Mobile</li>
-            </ul>
+                <ul>
+                    <For each={projects}>
+                        {(project) => <li>{project}</li>}
+                    </For>
+                </ul>
         </a>
     );
 };
@@ -29,26 +31,37 @@ const TechStack: Component = () => {
             label: 'Rust',
             link: 'https://img.icons8.com/?size=100&id=B1VZ9ZRl8Vwt&format=png&color=000000',
             alt: 'Rust',
+            projects: ['AntiCheating tools', 'Addresses fixer', 'WebAssembly', 'SeaORM Backend'],
         },
         {
             label: 'C/C++',
             link: 'https://img.icons8.com/color/96/000000/c-plus-plus-logo.png',
-            alt: 'React',
+            alt: 'C/C++',
+            projects: ['Own Chess AI', '3D Game w/ OpenGL', '2D Game with SFML', 'Personnal CLI'],
         },
         {
             label: 'JavaScript',
             link: 'https://img.icons8.com/color/96/000000/typescript.png',
             alt: 'JavaScript - TypeScript',
+            projects: ['SvelteKit', 'SolidJS', 'React', 'React Native App', 'NodeJS Backend', 'Discord Bots'],
         },
         {
             label: 'HTML',
             link: 'https://img.icons8.com/color/96/000000/html-5.png',
             alt: 'HTML',
+            projects: ['SvelteKit', 'SolidJS', 'React'],
         },
         {
             label: 'CSS',
             link: 'https://img.icons8.com/color/96/000000/css3.png',
             alt: 'CSS',
+            projects: ['CSS', 'SASS', 'TailwindCSS'],
+        },
+        {
+            label: 'SQL',
+            link: 'https://img.icons8.com/color/96/000000/sql.png',
+            alt: 'SQL',
+            projects: ['Postgres', 'MySQL', 'MongoDB'],
         },
     ];
 
@@ -57,7 +70,9 @@ const TechStack: Component = () => {
             <h2>Tech Stack</h2>
             <div class={styles.stackCarrousel}>
                 <For each={stack}>
-                    {({ label, link, alt }) => <StackDetails label={label} link={link} alt={alt} />}
+                    {({ label, link, alt, projects }) => (
+                        <StackDetails label={label} link={link} alt={alt} projects={projects} />
+                    )}
                 </For>
             </div>
         </div>
