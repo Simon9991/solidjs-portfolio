d1f1e956170c6963cf6c60c4779d14f33f40d6a4
Simon9991 <simon.bernabeu@epitech.eu>
2024-09-15 22:00:09 +0900

    feat: improved whole page for more modern

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,24 @@
-import { onCleanup, type Component, onMount } from 'solid-js';
+import type { Component } from 'solid-js';
 
-import styles from './App.module.scss';
-import TechStack from './components/TechStack';
+import { useDarkMode } from './components/UI/darkMode';
+import ToggleSwitch from './components/UI/ToggleSwitch';
+import TechUniverse from './components/TechUniverse';
+import Greeting from './components/Greeting';
 
 const App: Component = () => {
-    let greetingDiv: HTMLElement | null = null;
-    let header: HTMLElement | null = null;
-    let throttleTimeout: number | null = null;
-
-    function handleScroll() {
-        if (throttleTimeout || !greetingDiv || !header) return;
-
-        throttleTimeout = setTimeout(() => {
-            if (!greetingDiv || !header) return;
-
-            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-            const startFadingAt = 70; // Start fading after scrolling 100px
-            const endFadingAt = 170; // Fully transparent after scrolling 300px
-
-            if (scrollTop > startFadingAt) {
-                const opacity = 1 - (scrollTop - startFadingAt) / (endFadingAt - startFadingAt);
-                header.style.opacity = Math.max(opacity, 0).toString();
-            } else {
-                header.style.opacity = '1';
-            }
-            throttleTimeout = null;
-        }, 25); // 25ms delay
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    onMount(() => {
-        greetingDiv = document.getElementById('div-greeting');
-        header = document.getElementById('main-header');
-    });
-
-    onCleanup(() => {
-        window.removeEventListener('scroll', handleScroll);
-    });
+    const [isDarkMode, { toggle }] = useDarkMode();
 
     return (
-        <div id="app-div" class={styles.App}>
-            <header id="main-header" class={styles.header}>
-                <div id="div-greeting" class={styles.greeting}>
-                    <p>Hey! I am</p>
-                    <h1>Simon Bernabeu</h1>
-                    <p>a software engineer currently living in South Korea</p>
-                </div>
-
-                <div class={styles.scrollText}>
-                    <p>Scroll down to see more</p>
-                    {/* <FiChevronsRight size={23} class="rotate-90" /> */}
-                </div>
-            </header>
-
-            <div class={styles.otherInformation}>
-                <TechStack />
+        <div
+            class={`${isDarkMode() ? 'dark' : ''} min-h-screen relative overflow-auto transition-all dark:bg-gray-900 dark:text-white bg-gray-100 text-gray-800`}
+        >
+            <div class="w-full fixed z-10 dark:bg-black dark:bg-opacity-10 bg-indigo-500 bg-opacity-10 top-0 flex items-center justify-between px-8 backdrop-blur-sm h-20">
+                <h1 class="font-bold text-4xl mb-0">SB. Dev.</h1>
+                <ToggleSwitch />
             </div>
+
+            <Greeting />
+            <TechUniverse />
         </div>
     );
 };
