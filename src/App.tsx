677725a5a0fbd470e687a3c57c80e10a56f7428f
Simon9991 <simon.bernabeu@epitech.eu>
2024-10-17 23:23:48 +0900

    feat: added routing system!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { createResource, Show, Suspense, type Component } from 'solid-js';
+import { createResource, Show, Suspense, type Component } from "solid-js";
 
-import { useDarkMode } from './components/UI/DarkMode';
-import TechUniverse from './components/TechUniverse';
-import Greeting from './components/Greeting';
-import DarkModeToggle from './components/UI/DarkModeToggleSwitch';
-import LanguageSelect from './components/UI/LanguageSelect';
-import * as i18n from '@solid-primitives/i18n';
-import type { Dict } from './i18n/en';
-import { useLanguageSelect } from './components/Stores/LanguageSelectSignal';
-import LoadingIndicator from './components/UI/LoadingIndicator';
-import AboutMe from './components/AboutMe';
-import ContactMe from './components/ContactMe';
-import ParticlesEffect from './components/UI/ParticlesEffect';
+import { useDarkMode } from "./components/UI/DarkMode";
+import TechUniverse from "./components/TechUniverse";
+import Greeting from "./components/Greeting";
+import DarkModeToggle from "./components/UI/DarkModeToggleSwitch";
+import LanguageSelect from "./components/UI/LanguageSelect";
+import * as i18n from "@solid-primitives/i18n";
+import type { Dict } from "./i18n/en";
+import { useLanguageSelect } from "./components/Stores/LanguageSelectSignal";
+import LoadingIndicator from "./components/UI/LoadingIndicator";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import ParticlesEffect from "./components/UI/ParticlesEffect";
+import { Route, Router } from "@solidjs/router";
+import RouteTest from "./pages/Test";
 
-export type Locale = 'en' | 'ko' | 'fr';
+export type Locale = "en" | "ko" | "fr";
 export type RawDictionary = Dict;
 export type Dictionary = i18n.Flatten<RawDictionary>;
 
@@ -23,29 +25,13 @@ async function fetchDictionary(locale: Locale): Promise<Dictionary> {
 }
 
 const App: Component = () => {
-    const [isDarkMode] = useDarkMode();
-    const { language } = useLanguageSelect();
-    const [dict] = createResource(language, fetchDictionary);
-
-    dict();
-
     return (
-        <div class={isDarkMode() ? 'dark' : ''}>
-            <ParticlesEffect>
-                <div class="fixed inset-0 z-40 w-full bg-gradient-to-b dark:from-black from-white to-transparent to-100% h-44 flex justify-end p-6 gap-6">
-                    <LanguageSelect />
-                    <DarkModeToggle />
-                </div>
-                <Suspense fallback={<LoadingIndicator />}>
-                    <Show when={dict()}>
-                        <Greeting />
-                        <AboutMe />
-                        <TechUniverse />
-                        <ContactMe />
-                    </Show>
-                </Suspense>
-            </ParticlesEffect>
-        </div>
+        <>
+            <Greeting />
+            <AboutMe />
+            <TechUniverse />
+            <ContactMe />
+        </>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,69 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
-import { DarkModeProvider } from './components/UI/DarkMode';
-import { LanguageSelectProvider } from './components/Stores/LanguageSelectSignal';
+import "./index.css";
+import App from "./App";
+import { DarkModeProvider, useDarkMode } from "./components/UI/DarkMode";
+import {
+    LanguageSelectProvider,
+    useLanguageSelect,
+} from "./components/Stores/LanguageSelectSignal";
+import { Route, Router } from "@solidjs/router";
+import { createResource, type JSXElement, Show, Suspense } from "solid-js";
+import ParticlesEffect from "./components/UI/ParticlesEffect";
+import DarkModeToggle from "./components/UI/DarkModeToggleSwitch";
+import LanguageSelect from "./components/UI/LanguageSelect";
+import LoadingIndicator from "./components/UI/LoadingIndicator";
+import { type Dict, dict } from "./i18n/en";
+import * as i18n from "@solid-primitives/i18n";
+import RouteTest from "./pages/Test";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
     throw new Error(
-        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+        "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
     );
 }
+
+export type Locale = "en" | "ko" | "fr";
+export type RawDictionary = Dict;
+export type Dictionary = i18n.Flatten<RawDictionary>;
+
+async function fetchDictionary(locale: Locale): Promise<Dictionary> {
+    const dict: RawDictionary = (await import(`./i18n/${locale}.ts`)).dict;
+    return i18n.flatten(dict); // flatten the dictionary to make all nested keys available top-level
+}
+
+const Layout = (props: { children?: JSXElement }) => {
+    const [isDarkMode] = useDarkMode();
+    const { language } = useLanguageSelect();
+    const [dict] = createResource(language, fetchDictionary);
+
+    dict(); // trigger the fetch
+
+    return (
+        <div class={isDarkMode() ? "dark" : ""}>
+            <ParticlesEffect>
+                <div class="fixed inset-0 z-40 w-full bg-gradient-to-b dark:from-black from-white to-transparent to-100% h-44 flex justify-end p-6 gap-6">
+                    <LanguageSelect />
+                    <DarkModeToggle />
+                </div>
+                <Suspense fallback={<LoadingIndicator />}>
+                    <Show when={dict()}>{props.children}</Show>
+                </Suspense>
+            </ParticlesEffect>
+        </div>
+    );
+};
 
 render(
     () => (
         <DarkModeProvider>
             <LanguageSelectProvider>
-                <App />
+                <Router root={Layout}>
+                    <Route path="/" component={App} />
+                </Router>
             </LanguageSelectProvider>
         </DarkModeProvider>
     ),
