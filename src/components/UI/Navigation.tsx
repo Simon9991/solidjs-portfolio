0657ca1d61ae3e01c41966231118e350d7a3efe5
Simon9991 <simon.bernabeu@epitech.eu>
2024-10-21 20:48:36 +0900

    refactor(Navigation): removed dead code

--- src/components/UI/Navigation.tsx
+++ src/components/UI/Navigation.tsx
@@ -1,24 +1,27 @@
-import { A } from '@solidjs/router';
-import styles from '../../styles/Navigation.module.scss';
-import { createEffect, createSignal } from 'solid-js';
+import { A } from "@solidjs/router";
+import styles from "../../styles/Navigation.module.scss";
 
 export function Navigation() {
-    const [active, setActive] = createSignal('/');
-
-    createEffect(() => {
-        setActive(window.location.pathname);
-    });
-
     return (
         <nav>
             <ul class={styles.navList}>
                 <li>
-                    <A href="/" class={styles.navLink} activeClass={styles.activeLink} end>
+                    <A
+                        href="/"
+                        class={styles.navLink}
+                        activeClass={styles.activeLink}
+                        end
+                    >
                         Portfolio
                     </A>
                 </li>
                 <li>
-                    <A href="/posts" class={styles.navLink} activeClass={styles.activeLink} end>
+                    <A
+                        href="/posts"
+                        class={styles.navLink}
+                        activeClass={styles.activeLink}
+                        end
+                    >
                         Posts
                     </A>
                 </li>
